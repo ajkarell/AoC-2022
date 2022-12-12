da4661db85af157a7b90dae747ef128a777fd7c2
ajkarell <111962031+ajkarell@users.noreply.github.com>
2022-12-12 11:31:45 +0200

    add day 1

---
+++ 1/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.IO
+
+let toElfInventories (input: string) =
+    input.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun x ->
+        x.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map int)
+
+let calculateTotal (inventory: int []) = Array.sum inventory
+
+let input = File.ReadAllText "input.txt"
+
+let elvesSortedByCalories =
+    input
+    |> toElfInventories
+    |> Array.map calculateTotal
+    |> Array.sortDescending
+
+let elfMostCalories = Array.head elvesSortedByCalories
+printfn $"Part 1: {elfMostCalories}"
+
+let top3Total = elvesSortedByCalories |> Array.take 3 |> Array.sum
+printfn $"Part 2: {top3Total}"
