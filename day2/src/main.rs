1f709f6bd625e5e2e5e1d10bc4a00ff7e7c3122e
ajkarell <111962031+ajkarell@users.noreply.github.com>
2022-12-13 13:18:20 +0200

    day2: add part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,10 +1,10 @@
 enum Hand {
-    Rock = 1,
-    Paper = 2,
-    Scissors = 3,
+    Rock,
+    Paper,
+    Scissors,
 }
 
-fn to_hands(round: &str) -> (Hand, Hand) {
+fn to_hands_part1(round: &str) -> (Hand, Hand) {
     let opponent = match round.chars().nth(0).unwrap() {
         'A' => Hand::Rock,
         'B' => Hand::Paper,
@@ -16,6 +16,32 @@ fn to_hands(round: &str) -> (Hand, Hand) {
         'X' => Hand::Rock,
         'Y' => Hand::Paper,
         'Z' => Hand::Scissors,
+        _ => panic!("Invalid input."),
+    };
+
+    (opponent, me)
+}
+
+fn to_hands_part2(round: &str) -> (Hand, Hand) {
+    let opponent = match round.chars().nth(0).unwrap() {
+        'A' => Hand::Rock,
+        'B' => Hand::Paper,
+        'C' => Hand::Scissors,
+        _ => panic!("Invalid input."),
+    };
+
+    let me = match (&opponent, round.chars().nth(2).unwrap()) {
+        (Hand::Rock, 'X') => Hand::Scissors,
+        (Hand::Rock, 'Y') => Hand::Rock,
+        (Hand::Rock, 'Z') => Hand::Paper,
+
+        (Hand::Paper, 'X') => Hand::Rock,
+        (Hand::Paper, 'Y') => Hand::Paper,
+        (Hand::Paper, 'Z') => Hand::Scissors,
+
+        (Hand::Scissors, 'X') => Hand::Paper,
+        (Hand::Scissors, 'Y') => Hand::Scissors,
+        (Hand::Scissors, 'Z') => Hand::Rock,
         _ => panic!("Invalid input."),
     };
 
@@ -46,11 +72,18 @@ fn play(hands: (Hand, Hand)) -> i32 {
 fn main() {
     let input = std::fs::read_to_string("input.txt").expect("Input not found.");
 
-    let total_points: i32 = input
+    let total_points_part1: i32 = input
         .lines()
-        .map(|round| to_hands(round))
+        .map(|round| to_hands_part1(round))
         .map(|hands| play(hands))
         .sum();
 
-    println!("{total_points}");
+    let total_points_part2: i32 = input
+        .lines()
+        .map(|round| to_hands_part2(round))
+        .map(|hands| play(hands))
+        .sum();
+
+    println!("Part 1: {total_points_part1}");
+    println!("Part 2: {total_points_part2}");
 }
