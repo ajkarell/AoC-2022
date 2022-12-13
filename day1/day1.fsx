b571b455173871c2b3c248e4a9f39f97c03f395b
ajkarell <111962031+ajkarell@users.noreply.github.com>
2022-12-13 11:40:49 +0200

    day1: refactor

--- day1/day1.fsx
+++ day1/day1.fsx
@@ -2,9 +2,9 @@
 open System.IO
 
 let toElfInventories (input: string) =
-    input.Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-    |> Array.map (fun x ->
-        x.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+    input.Split(Environment.NewLine + Environment.NewLine)
+    |> Array.map (fun inventoryString ->
+        inventoryString.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
         |> Array.map int)
 
 let calculateTotal (inventory: int []) = Array.sum inventory
@@ -17,8 +17,8 @@ let elvesSortedByCalories =
     |> Array.map calculateTotal
     |> Array.sortDescending
 
-let elfMostCalories = Array.head elvesSortedByCalories
-printfn $"Part 1: {elfMostCalories}"
+let topCalories = Array.head elvesSortedByCalories
+printfn $"Part 1: {topCalories}"
 
 let top3Total = elvesSortedByCalories |> Array.take 3 |> Array.sum
 printfn $"Part 2: {top3Total}"
